713595499a95efc675093039b6c882916b141e1b
Ayushganeshi <ayushlatehar@gmail.com>
2025-06-30 22:59:37 +0530

    Luntik and Subsequences

---
+++ Luntik and Subsequences.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
+
+//  AUTHOR:->Ayush Kumar Gupta 
+//       !! CHESS & CODE!! 
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    int zeroes = 0, ones = 0;
+    for (int i = 0; i < n; i++) {
+        int x; 
+        cin >> x;
+        if (x == 0) zeroes++;
+        else if (x == 1) ones++;
+    }
+    
+    int total = ones * (1LL << zeroes);
+    cout << total << "\n";
+}
+
+int32_t main() {
+    fast_io();
+    
+    int t; 
+    cin >> t;  
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
