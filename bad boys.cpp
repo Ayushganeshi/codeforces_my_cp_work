1f8c0c2abdf5414ac9b7bf82208cf81ee43ab9d8
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-01 21:07:14 +0530

    bad boys

---
+++ bad boys.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
+
+//  AUTHOR:->Ayush Kumar Gupta 
+//       !! CHESS & CODE!! 
+
+void solve() {
+    int m, n, x, y;
+    cin >> m >> n >> x >> y;
+
+    
+    cout << 1 << " " << 1 << " " << m << " " << n << endl;
+}
+
+int32_t main() {
+    fast_io();
+    
+    int t; 
+    cin >> t;  
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
