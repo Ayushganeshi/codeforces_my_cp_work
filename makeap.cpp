7d2018c9715eda147afbac15c218812bb491c68d
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-01 21:11:01 +0530

    make_ap

--- makeap.cpp
+++ makeap.cpp
@@ -1,19 +1,58 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
-int main() {
-   int num,ans,rem,mul;
-        cout<<"enter the number";
-        cin>>num;
-        ans=0;
-        mul=1;
-        while(num>0){
-            rem=num%2;
-            num/=2;
-            ans=rem*mul+ans;
-            mul*=10;
+//  AUTHOR:->Ayush Kumar Gupta 
+//       !! CHESS & CODE!! 
 
-        }
-        cout<<ans<<endl;
+
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int d1 = b - a, d2 = c - b;
+    if (d1 == d2) {
+        cout << "YES\n";
+        return;
+    }
+
+    
+    int d = (c - a) / 2;
+    int x = a + d;
+    if ((c - a) % 2 == 0 && x % b == 0 && x != 0 && x >= b) {
+        cout << "YES\n";
+        return;
+    }
+
+    
+    d = b - a;
+    x = b + d;
+    if (x % c == 0 && x != 0 && x >= c) {
+        cout << "YES\n";
+        return;
+    }
+
+    
+    d = c - b;
+    x = b - d;
+    if (x % a == 0 && x != 0 && x >= a) {
+        cout << "YES\n";
+        return;
+    }
+
+    cout << "NO\n";
+}
+
+int32_t main() {
+    fast_io();
+    
+    int t; 
+    cin >> t;  
+    while (t--) {
+        solve();
+    }
+
     return 0;
 }
