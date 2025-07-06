868c3a8fd33cd42238fec582f98374beab8c1a54
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-06 22:55:19 +0530

    Add files via upload

---
+++ add_digit_until_single_digit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int ans=0;
+    int num,rem;
+    cout<<"enter the number";
+    cin>>num;
+    while(num>0){
+        rem=num%10;
+        num=num/10;
+        ans=ans+rem;
+        if(num==0&&ans>9)
+        {
+            num=ans;
+            ans=0;
+        }
+        
+
+        
+    }
+    cout<<ans;
+}
