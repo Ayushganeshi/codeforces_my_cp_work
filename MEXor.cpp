50ebaefd0a662c900ca5c7fd3a2428c7f0efcd49
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-07 21:40:01 +0530

    Add files via upload

---
+++ MEXor.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
+
+// AUTHOR:->Ayush Kumar Gupta 
+//       !! CHESS & CODE!!
+
+void solve() {
+    int mex, x;
+    cin >> mex >> x;
+
+    int c = 0;
+
+    
+    int xor_all = 0;
+    int rem = (mex - 1) % 4;
+    if (rem == 0) xor_all = (mex - 1);
+    else if (rem == 1) xor_all = 1;
+    else if (rem == 2) xor_all = (mex - 1) + 1;
+    else xor_all = 0;
+
+    int temp = xor_all ^ x;
+    int ans = 0;
+    
+    if (xor_all == x) ans = mex;
+    else if (temp == mex) ans = mex + 2;
+    else ans = mex + 1;
+
+    cout << ans <<endl;
+}
+
+int32_t main() {
+    fast_io();
+    
+    int t; 
+    cin >> t;  
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
