770749a79c41eb6e9c6623361924a618336ec8b9
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-08 23:51:02 +0530

    Add files via upload

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int num,ans,rem,mul;
+        cout<<"enter the number";
+        cin>>num;
+        ans=0;
+        mul=1;
+        while(num>0){
+            rem=num%10;
+            num/=10;
+            ans=rem*mul+ans;
+            mul*=2;
+
+        }
+        cout<<ans<<endl;
+    return 0;
+}
