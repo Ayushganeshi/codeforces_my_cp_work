032ce8202b109725548ca1edd5e60fe549abccfc
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-08 23:50:06 +0530

    Add files via upload

---
+++ armstrong num.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int store,ans,rem,num;
+    ans=0;
+    cout<<"enter the number:";
+    cin>>num;
+    store=num;
+    while(num>0)
+    {
+        rem=num%10;
+        num/=10;
+        ans+=rem*rem*rem;
+    }
+    cout<<ans;
+    
+    if(ans=store){
+        cout<<"armstrong number";
+    }
+    else{
+        cout<<"not an armstrong number";
+    }
+    return 0;
+
+
+    
+}
