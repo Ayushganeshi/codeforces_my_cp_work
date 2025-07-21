cf08aee89b60a27db45f7290976338a9588d0da7
Ayushganeshi <ayushlatehar@gmail.com>
2025-07-21 22:18:47 +0530

    Add files via upload

---
+++ cpbasic2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>a;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        a.push_back(x);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
