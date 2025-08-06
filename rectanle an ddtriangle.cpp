a750e50216390ccad222bced5c8b4173a9ab5b0d
Ayushganeshi <ayushlatehar@gmail.com>
2025-08-07 02:36:43 +0530

    Update rectanle an ddtriangle.cpp

---
+++ rectanle an ddtriangle.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
+
+//  AUTHOR:->Ayush Kumar Gupta 
+//       !! CHESS & CODE!! 
+
+// Problem: Triangles on a Rectangle
+// Link: https://codeforces.com/problemset/problem/1620/B
+
+void solve() {
+    int w, h;
+    cin >> w >> h;
+    
+    int n, area = 0;
+    
+    for (int j = 0; j < 4; j++) {
+        cin >> n;
+        int s, e;
+        for (int i = 0; i < n; i++) {
+            int t;
+            cin >> t;
+            if (i == 0) s = t;
+            if (i == n - 1) e = t;
+        }
+        if (j < 2) {
+            area = max(area, (e - s) * h);
+        } else {
+            area = max(area, (e - s) * w);
+        }
+    }
+    
+    cout << area << "\n";
+}
+
+int32_t main() {
+    fast_io();
+    
+    int t; 
+    cin >> t;  
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
