8afe37f84abb6f5eb8123155f1011797f0276939
Ayushganeshi <ayushlatehar@gmail.com>
2025-08-24 10:51:58 +0530

    Update e,gcd unlucky.cpp

---
+++ e,gcd unlucky.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+int solve(){
+}
+
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+        int n;
+        cin>>n; int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int x;int ans = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==i+1) continue;
+            x = abs(arr[i]-(i+1));
+            ans = gcd(x,ans);
+        }
+        cout<<ans<<endl;
+	}
+}
