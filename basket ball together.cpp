43ceddc40181942fd87550e9b04a2201ba3bd9bd
Ayushganeshi <ayushlatehar@gmail.com>
2025-08-30 19:13:29 +0530

    Update basket ball together.cpp

--- basket ball together.cpp
+++ basket ball together.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define int long long
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
-//  AUTHOR:->Ayush Kumar Gupta 
+//  AUTHOR:->Ayush Kumar Gupt
 //       !! CHESS & CODE!! 
 
 
@@ -40,3 +40,4 @@ int32_t main() {
 
     return 0;
 }
+
