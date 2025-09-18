0f563cc65830da61c904b51138454430af6e0beb
Ayushganeshi <ayushlatehar@gmail.com>
2025-09-18 17:58:26 +0530

    Update Helmet in the night light.cpp

--- Helmet in the night light.cpp
+++ Helmet in the night light.cpp
@@ -12,7 +12,7 @@ void solve() {
     vector<int> a(n), b(n);
     vector<pair<int, int>> v;
 
-    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a [i];
     for (int i = 0; i < n; i++) {
         cin >> b[i];
         v.push_back({b[i], a[i]});
@@ -46,4 +46,5 @@ int32_t main() {
 
     return 0;
 }
+
 
