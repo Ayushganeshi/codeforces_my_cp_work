c5521e6b339492b8db9ffa91b0d3564ef706bb31
Ayushganeshi <ayushlatehar@gmail.com>
2025-12-12 21:14:47 +0530

    Update Rasberies.cpp

--- Rasberies.cpp
+++ Rasberies.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
 //  AUTHOR:->Ayush Kumar Gupt
-//       !! CHESS & CODEEE &&& CODEEEE
+//       !! CHESS & CODEEE 
 
 void solve() {
     int n, k;
@@ -47,6 +47,7 @@ int32_t main() {
 
     return 0;
 }
+
 
 
 
