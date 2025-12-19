776f087e7c19cb3773766c1632e0f33ce06c8bca
Ayushganeshi <ayushlatehar@gmail.com>
2025-12-19 19:45:18 +0530

    Update Monster.cpp

--- Monster.cpp
+++ Monster.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
 //  AUTHOR:->Ayush Kumar Gupta 
-//       !! CHESS & CODE!! 
+//       !! CHESS &&&& CODE!! 
 
 class cmp {
 public: 
@@ -51,4 +51,5 @@ int32_t main() {
 
     return 0;
 }
+
 
