97db0603f7be70ceb346b194f7d63d55ae1aee73
Ayushganeshi <ayushlatehar@gmail.com>
2026-01-05 12:05:43 +0530

    Update exciting bet.cpp

--- exciting bet.cpp
+++ exciting bet.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
 //  AUTHOR:->Ayush Kumar Gupta 
-//       !! CHESS & CODE!! 
+//       !! CHESS & CODE!
 
 void solve() {
     int a, b;
@@ -35,5 +35,6 @@ int32_t main() {
 
     return 0;
 }
+
 
 
