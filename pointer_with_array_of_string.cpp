e7ef5b2e9a04ad46e783d2f496570b6027fad84c
Ayushganeshi <ayushlatehar@gmail.com>
2026-01-11 18:26:57 +0530

    Update pointer_with_array_of_string.cpp

--- pointer_with_array_of_string.cpp
+++ pointer_with_array_of_string.cpp
@@ -5,10 +5,12 @@ int main()
     const char* arr[]={"sumit","ayush","rajveer","ravikant"};
     const char** p=arr;
     for(int i=0;i<4;i++)
+        
     {
         cout<<*(p+i)<<endl;
     }
 
     return 0;
+
 
 }
