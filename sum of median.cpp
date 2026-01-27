1909b2d48b6be4814e6b9a0344465f8d25045c23
Ayushganeshi <ayushlatehar@gmail.com>
2026-01-27 11:04:11 +0530

    Update sum of median.cpp

--- sum of median.cpp
+++ sum of median.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define int long long
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0);
 
-//  AUTHOR:->Ayush Kumar Gupta 
+//  AUTHOR:->Ayush Kumar G
 //       !! CHESS &    CODE!! 
 
 void solve() {
@@ -38,4 +38,5 @@ int32_t main() {
 
     return 0;
 }
+
 
