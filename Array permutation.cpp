2cc5af9edd45ae17434640019c5465fa7c78bd5f
Ayushganeshi <ayushlatehar@gmail.com>
2026-03-09 12:34:38 +0530

    Create Array permutation.cpp

---
+++ Array permutation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+ 
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define len(a) int(a.size())
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+ 
+using namespace std;
+ 
+const int mod = 998244353;
+ 
+const int N = 1e6 + 228;
+ 
+pair<int, int> zero = {-1, N};
+ 
+ 
+void solve() {
+    int n;
+    cin >> n;
+    map<int, int> pos;
+    for (int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        pos[a] = i;
+    }
+    int lst = -1;
+    vector<int> b(n);
+    for (int i = 0; i < n; ++i) cin >> b[i];
+    for (int i = 0; i < n; ++i) {
+        if (lst > pos[b[i]]) {
+            cout << "NO\n";
+            return;
+        }
+        lst = pos[b[i]];
+    }
+    cout << "YES\n";
+}
+ 
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
