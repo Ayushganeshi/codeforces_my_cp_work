140f36172098f211767419e4bd5f0ad646ce090d
Ayushganeshi <ayushlatehar@gmail.com>
2026-04-07 23:54:41 +0530

    Create MAximum product of three number.cpp

---
+++ MAximum product of three number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n=nums.size();
+
+        sort(nums.begin(),nums.end(),greater<int>());
+        int res=nums[0]*nums[1]*nums[2];
+        int res1=nums[n-1]*nums[n-2]*nums[0];
+        int ans=max(res,res1);
+        return ans;
+    }
+};
