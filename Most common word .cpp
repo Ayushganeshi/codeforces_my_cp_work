7a44551e4055222dd0985239e36db1d60393088d
Ayushganeshi <ayushlatehar@gmail.com>
2026-04-09 01:00:51 +0530

    Create Most common word .cpp

---
+++ Most common word .cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+           for(auto & ch :paragraph)
+           {
+            if(!isalpha(ch))
+            ch=' ';
+            else
+            ch=tolower(ch);
+            
+           }
+           unordered_set<string>st;
+           for(auto & wor :banned)
+           {
+            st.insert(wor);
+           }
+           stringstream ss(paragraph);
+           unordered_map<string,int>mp;
+           string word;
+           string ans;
+           int mxcnt=0;
+           while(ss>>word)
+           {
+            if(st.find(word)==st.end())
+            {
+                mp[word]++;
+            }
+           }
+           int m=INT_MIN;
+           for(auto &it: mp)
+           {
+            if(it.second>m)
+            {
+                ans=it.first;
+                m=it.second;
+            }
+           }
+           return ans;
+           
+
+
+    }
+};
