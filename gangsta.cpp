a3ce493718ed9011430a9dceb03503f6813e03f0
Ayushganeshi <ayushlatehar@gmail.com>
2026-05-27 22:03:21 +0530

    Create gangsta.cpp

---
+++ gangsta.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+int const maxn = 2e5 + 5;
+int pref[maxn];
+
+main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll ans = 0;
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i];
+            if (s[i] == '0') pref[i + 1]--;
+            else pref[i + 1]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            ans += (ll)i * (n - i + 1);
+        }
+        sort(pref, pref + n + 1);
+        for (int i = 0; i <= n; i++) {
+            ans += (ll)pref[i] * (i - (n - i));
+        }
+        cout << ans / 2 << '\n';
+    }
+    return 0;
+}
