ef7d6ecf6147cf082271965170909dd78076d484
Ayushganeshi <ayushlatehar@gmail.com>
2026-06-15 10:04:56 +0530

    Implement reversePrefix function in Solution class

---
+++ reverse prefix .cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int n=word.length();
+        int idx=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(word[i]==ch)
+            {
+                idx=i;
+                break;
+            }
+        }
+        reverse(word.begin(),word.begin()+idx+1);
+        return word;
+    }
+};
